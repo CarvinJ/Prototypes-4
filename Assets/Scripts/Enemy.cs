dbba2978a95c73da229cee685d59a8cb601ef642
CarvinJ <113186066+CarvinJ@users.noreply.github.com>
2024-01-08 12:32:41 -0500

    part 4.4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public int waveNumber = 1;
     private Rigidbody enemyRb;
     private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +22,7 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position). normalized;
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10) { Destroy(GameObject);}
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public int waveNumber = 1;
+    public int enemyCount;
     private float spawnRange = 9;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0) 
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition ()
